1c7b34706e222ff9da33593b2e168f63455f971f
Logan Somoza <logansomoza@gmail.com>
2025-03-14 22:58:20 -0700

    Update BusSystemIndexer.cpp

--- proj4/src/BusSystemIndexer.cpp
+++ proj4/src/BusSystemIndexer.cpp
@@ -149,19 +149,20 @@ bool CBusSystemIndexer::SImplementation::RoutesByNodeIDs(TNodeID src, TNodeID de
 
     for (std::size_t i = 0; i < busSystemPtr->RouteCount(); ++i) {
         auto route = busSystemPtr->RouteByIndex(i);
-        if (route) {
-            int hasSrc = 0, hasDest = 0;
-            for (std::size_t j = 0; j < route->StopCount(); ++j) {
-                if (route->GetStopID(j) == srcID) {
-                    hasSrc = 1;
-                }
-                if (route->GetStopID(j) == destID) {
-                    hasDest = 1;
-                }
-                if (hasSrc + hasDest == 2) {
-                    outRoutes.insert(route);
-                    break;
-                }
+        if (!route) {
+            continue;
+        }
+        int hasSrc = 0, hasDest = 0;
+        for (std::size_t j = 0; j < route->StopCount(); ++j) {
+            if (route->GetStopID(j) == srcID) {
+                hasSrc = 1;
+            }
+            if (route->GetStopID(j) == destID) {
+                hasDest = 1;
+            }
+            if (hasSrc + hasDest == 2) {
+                outRoutes.insert(route);
+                break;
             }
         }
     }
